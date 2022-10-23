48a95d41aae4698834064db3730ae0d0adc059c8
Critian Andrada <cristianandrada286@gmail.com>
2022-10-23 17:01:36 -0300

    Desafio Final

--- desafio final/js/app.js
+++ desafio final/js/app.js
@@ -1,12 +1,20 @@
-let divProductos = document.getElementById("songs")
+let divProductos = document.getElementById("animes")
 
-//▼Fetch a pagina Canciones Metallica ►primer API con método GET
+//▼Fetch a pagina Anime Naruto ►primer API con método GET
 
-fetch('http://www.songsterr.com/a/ra/songs/byartists.xml?artists=Metallica')
+fetch('https://animechan.vercel.app/api/quotes/anime?title=naruto&page=2')
     .then(response => response.json())
-    .then((datos) => {
-        console.log(datos)
-        for (let metallica of datos.NSArray) {
-            console.log(metallica)
+    .then((quotes) => {
+        console.log(quotes)
+        for (let anime of quotes) {
+            let divAnimes = document.createElement("div")
+            divAnimes.innerHTML = `<div id="animes">
+                <div class="card-body">
+                    <h5 class="card-title">${anime.character}</h5>
+                    <p class="">${anime.quote}</p>
+                    <p class="">Anime: ${anime.anime}</p>
+                </div>
+            </div>`
+            divProductos.appendChild(divAnimes)
         }
-    })
+    });
